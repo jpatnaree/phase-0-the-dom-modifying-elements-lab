3c93fbc36b882a6db0e6635c1ba5651a9a4e3d52
Patnaree Johnson <j.patnaree.t@gmail.com>
2023-08-08 20:02:26 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = "<h1>Simone is the champion</h1>";
